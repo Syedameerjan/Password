74e67fe00f2aaccb7e8e290879066f59ae5bbc73
Syedameerjan <133519135+Syedameerjan@users.noreply.github.com>
2024-10-28 02:22:33 +0530

    Update userSlice.js

--- src/Toolkit/userSlice.js
+++ src/Toolkit/userSlice.js
@@ -1,24 +1,23 @@
 import { createSlice } from "@reduxjs/toolkit";
-const INITIAL_STATE={
 
-    userDetails:JSON.parse(localStorage.getItem('user'))??{},
-}
+const INITIAL_STATE = {
+    userDetails: JSON.parse(localStorage.getItem('user')) ?? {},
+};
 
+const outSlice = createSlice({
+    name: 'userOut',
+    initialState: INITIAL_STATE,
 
-
-
-const commonSlice=createSlice({
-    name:'user',
-    initialState:INITIAL_STATE,
-
-    reducers:{
-        setUserDetails:(state,action)=>{
-            state.userDetails=action.payload
+    reducers: {
+        setUserDetails: (state, action) => {
+            state.userDetails = action.payload;
         },
-      
-    }
+        clearUserDetails: (state) => {
+            state.userDetails = {}; // Reset user details
+        },
+        // Add any other actions you need, e.g., setUserRole
+    },
+});
 
-})
-
-export const {setUserDetails,setUserRole}=commonSlice.actions
-export default commonSlice.reducer
+export const { setUserDetails, clearUserDetails } = outSlice.actions;
+export default outSlice.reducer;
